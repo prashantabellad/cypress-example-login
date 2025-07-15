f1c50254fe8db81f3ac8dd43635d1faa1bad2fcb
Prashant Bellad <prashant@capbase.com>
2025-07-15 19:25:35 +0530

    added cypress tags

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 const dotenv = require("dotenv");
 const cypressDotenv = require("cypress-dotenv");
+const { tagify } = require("cypress-tags");
 
 const envFile = "./cypress/.env";
 dotenv.config({ path: envFile });
@@ -9,6 +10,7 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: process.env.CYPRESS_BASE_URL,
     setupNodeEvents(on, config) {
+      on("file:preprocessor", tagify(config));
       // Load environment variables
       config = cypressDotenv(config, {
         path: envFile,
--- cypress/e2e/biorev/user-authorization.cy.js
+++ cypress/e2e/biorev/user-authorization.cy.js
@@ -4,25 +4,40 @@ import { validUser } from "../../support/testdata";
 describe("User Authorization for biorev", () => {
   const { username, password } = validUser;
   context("Valid Login", () => {
-    it("User should be able to successful login with valid credentials", () => {
-      cy.loginUser(validUser);
-      cy.validateSuccessfulLogin();
-    });
+    it(
+      ["smoke"],
+      "User should be able to successful login with valid credentials",
+      () => {
+        cy.loginUser(validUser);
+        cy.validateSuccessfulLogin();
+      }
+    );
   });
   context("Invalid Login", () => {
-    it("User should not be able to login with invalid username", () => {
-      cy.loginUser({ username: "invalidUser", password });
-      cy.validateErrorMessage("Your username is invalid!");
-    });
+    it(
+      ["smoke"],
+      "User should not be able to login with invalid username",
+      () => {
+        cy.loginUser({ username: "invalidUser", password });
+        cy.validateErrorMessage("Your username is invalid!");
+      }
+    );
 
     it("User should not be able to login with invalid password", () => {
       cy.loginUser({ username, password: "incorrectPassword" });
       cy.validateErrorMessage("Your password is invalid!");
     });
 
-    it("User should not be able to login with invalid username and password", () => {
-      cy.loginUser({ username: "invalidUser", password: "incorrectPassword" });
-      cy.validateErrorMessage("Your username is invalid!");
-    });
+    it(
+      ["smoke"],
+      "User should not be able to login with invalid username and password",
+      () => {
+        cy.loginUser({
+          username: "invalidUser",
+          password: "incorrectPassword",
+        });
+        cy.validateErrorMessage("Your username is invalid!");
+      }
+    );
   });
 });
